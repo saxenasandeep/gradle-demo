f7a9c281e9ffe6049e87e91b314d2fc50070fa52
Sanjeev <sanrocks123@gmail.com>
2018-01-14 14:38:34 +0530

    build correction, port changed to 1000

--- src/main/java/com/sanjeev/gradledemo/main/GradleDemoMain.java
+++ src/main/java/com/sanjeev/gradledemo/main/GradleDemoMain.java
@@ -4,13 +4,9 @@
 
 package com.sanjeev.gradledemo.main;
 
-import java.io.PrintStream;
-
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
 
 /**
  * Main entry point
@@ -30,16 +26,6 @@ public class GradleDemoMain {
      */
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(GradleDemoMain.class);
-
-        app.setBanner(new Banner() {
-            @Override
-            public void printBanner(Environment env, Class<?> classz, PrintStream ps) {
-                ps.println("*******************************************************************************");
-                ps.println(
-                        "                                Welcome to SpringBoot Application                              ");
-                ps.println("*******************************************************************************");
-            }
-        });
         app.run(args);
     }
 
